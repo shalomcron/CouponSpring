0967e487e2b58a76832fd9e8d35e49147e97c47e
shalom <shalomcron@gmail.com>
2022-07-24 23:23:01 +0300

    add files

---
+++ src/main/java/com/coupon/CouponSpring/clr/on/services/Test03CompanyServiceTest.java
@@ -0,0 +1,48 @@
+package com.coupon.CouponSpring.clr.on.services;
+
+import com.coupon.CouponSpring.bean.Company;
+import com.coupon.CouponSpring.bean.Customer;
+import com.coupon.CouponSpring.services.clients.AdminService;
+import com.coupon.CouponSpring.services.clients.CompanyException;
+import com.coupon.CouponSpring.services.clients.CompanyService;
+import com.coupon.CouponSpring.services.login.ClientType;
+import com.coupon.CouponSpring.services.login.LoginManager;
+import com.coupon.CouponSpring.utils.BeanFactoryUtils;
+import com.coupon.CouponSpring.utils.Print;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class Test03CompanyServiceTest implements CommandLineRunner {
+    CompanyService companyService = null;
+
+    @Autowired
+    private LoginManager loginManager;
+
+    @Override
+    public void run(String... args) {
+        Print.printMainCaption("Start company test");
+        Print.printMainCaption("Start company login Tests");
+        loginTests();
+    }
+
+    private void loginTests() {
+        try {
+            Print.printSubCaption("trying to log-in company");
+            companyService = (CompanyService) loginManager.login("admin1", "admin1", ClientType.Company);
+        } catch (Exception e) {
+            Print.printException("fail to log-in", e);
+        }
+        try {
+            companyService = (CompanyService) loginManager.login("TARA-mail@gmail.com", "TARA-password", ClientType.Company);
+            Print.printSubCaption("log-in company successfully");
+        } catch (Exception e) {
+            Print.printException("fail to log-in", e);
+        }
+    }
+}
+
+
