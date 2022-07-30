c41002e068ae806bdd43caa1d17a659eca718ea1
shalom <shalomcron@gmail.com>
2022-07-30 21:15:10 +0300

    add files

--- src/main/java/com/coupon/CouponSpring/repos/CouponRepository.java
+++ src/main/java/com/coupon/CouponSpring/repos/CouponRepository.java
@@ -4,9 +4,13 @@ import com.coupon.CouponSpring.bean.Company;
 import com.coupon.CouponSpring.bean.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface CouponRepository extends JpaRepository<Coupon, Integer> {
      boolean existsByTitleAndCompany(String title, Company company);
 
     Coupon findByIdAndCompany(int couponId, Company company);
+
+    List<Coupon> findByCompany(Company company);
 }
